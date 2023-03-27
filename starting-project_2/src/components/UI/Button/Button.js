ffc9cc9449d0267056015e87204cf6d82d17dfef
Mubasher <mobi_20_11@yahoo.com>
2023-03-28 01:02:31 +0200

    update with new method

--- starting-project_2/src/components/UI/Button/Button.js
+++ starting-project_2/src/components/UI/Button/Button.js
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import React from "react";
+import styles from "./Button.module.css";
+
+/* import styled from "styled-components";
 const Button = styled.button`
   width: 100%;
   font: inherit;
@@ -23,5 +26,13 @@ const Button = styled.button`
   border-color: #ac0e77;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
 }`;
+ */
 
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 export default Button;
