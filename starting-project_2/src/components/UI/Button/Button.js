7a66c27b002732376c417141236c181cc3ac9bfd
Mubasher <mobi_20_11@yahoo.com>
2023-03-28 00:34:30 +0200

    styled component added

--- starting-project_2/src/components/UI/Button/Button.js
+++ starting-project_2/src/components/UI/Button/Button.js
@@ -1,8 +1,6 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Button=styled.button`
-
+import styled from "styled-components";
+const Button = styled.button`
+  width: 100%;
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -11,26 +9,19 @@ const Button=styled.button`
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
   cursor: pointer;
 
+@media (min-width: 768px){
+  width:auto;
+}
 
-
+&:focus {
   outline: none;
+}
 
-
-.button:hover,
-.button:active {
+&:hover,
+&:active {
   background: #ac0e77;
   border-color: #ac0e77;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-`
+}`;
 
-
-
-/* const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
- */
 export default Button;
